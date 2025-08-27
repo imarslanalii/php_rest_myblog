4002e85be0e5d754e39a63bc40139d550e082855
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-08-27 04:26:13 -0700

    Update read.php

--- api/category/read.php
+++ api/category/read.php
@@ -4,7 +4,6 @@
   header('Content-Type: application/json');
   include_once '../../config/Database.php';
   include_once '../../models/Category.php';
-
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
