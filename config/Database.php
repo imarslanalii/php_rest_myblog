778b17d09bbefbf95c135a18ed3d1d171748a7e4
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-09-11 05:24:02 -0700

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -9,7 +9,6 @@
     // DB Connect
     public function connect() {
       $this->conn = null;
-
       try { 
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
