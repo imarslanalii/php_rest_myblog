2b1d6c98fd380e3357a5d3951da9724e422282bd
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-09-18 03:35:05 -0700

    Update create.php

--- api/post/create.php
+++ api/post/create.php
@@ -4,7 +4,6 @@
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
 
