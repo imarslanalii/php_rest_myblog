bd7e4a8fc92226e3f943a0b7076d66770859aa5c
imarslanalii <122626416+imarslanalii@users.noreply.github.com>
2025-10-01 05:18:05 -0700

    Update delete.php

--- api/post/delete.php
+++ api/post/delete.php
@@ -6,7 +6,6 @@
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
-
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
